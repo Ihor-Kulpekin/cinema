d5e950a0f8e4043271e54730af2369c3dfff28e6
Kulpa1 <kulpekin20@gmail.com>
2019-10-05 14:07:20 +0300

    added service for "Sms"

---
+++ src/test/java/com/webencyclop/demo/service/implementation/forUser/SmsServiceImplTest.java
@@ -0,0 +1,29 @@
+package com.webencyclop.demo.service.implementation.forUser;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SmsServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void sentAndSaveSMS() {
+    }
+
+    @Test
+    public void getSMSById() {
+    }
+
+    @Test
+    public void removeSMS() {
+    }
+
+    @Test
+    public void findSMSByTos() {
+    }
+}
