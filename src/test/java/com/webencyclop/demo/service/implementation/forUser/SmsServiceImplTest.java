a84f87e6f2d5858ec1d59b19d31415fab6daf2c6
Kulpa1 <kulpekin20@gmail.com>
2019-10-05 14:11:50 +0300

    added test for SmsService

--- src/test/java/com/webencyclop/demo/service/implementation/forUser/SmsServiceImplTest.java
+++ src/test/java/com/webencyclop/demo/service/implementation/forUser/SmsServiceImplTest.java
@@ -1,29 +1,70 @@
 package com.webencyclop.demo.service.implementation.forUser;
 
+import com.webencyclop.demo.model.Sms;
+import com.webencyclop.demo.service.interfaces.forUser.SmsService;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
+import java.util.Date;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@SpringBootTest
+@Transactional
+@Rollback
+@RunWith(SpringRunner.class)
 public class SmsServiceImplTest {
 
+    @Autowired
+    private SmsService smsServiceInterface;
+
+
+    private Sms expectedSMS;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        expectedSMS = new Sms();
+        expectedSMS.setTos("tos");
+        expectedSMS.setFroms("froms");
+        expectedSMS.setContents("contents");
+        expectedSMS.setSubjects("subjects");
+        expectedSMS.setDateGettingMessage(new Date());
+        smsServiceInterface.sentAndSaveSMS(expectedSMS);
     }
 
     @Test
-    public void sentAndSaveSMS() {
+    public void sentAndSaveSMSTest() {
+        Sms sms = smsServiceInterface.getSMSById(expectedSMS.getId());
+        assertEquals(expectedSMS,sms);
     }
 
     @Test
-    public void getSMSById() {
+    public void getSMSByIdTest() {
+        List<Sms> smsList = smsServiceInterface.findSMSByTos(expectedSMS.getTos());
+        Sms smsFromList = smsList.get(0);
+        Sms smsGotById = smsServiceInterface.getSMSById(smsFromList.getId());
+        assertNotNull(smsGotById);
     }
 
     @Test
-    public void removeSMS() {
+    public void removeSMSTest() {
+        assertEquals(1L,smsServiceInterface.findSMSByTos(expectedSMS.getTos()).size());
+        smsServiceInterface.removeSMS(expectedSMS.getId());
+        assertEquals(0L,smsServiceInterface.findSMSByTos(expectedSMS.getTos()).size());
     }
 
     @Test
-    public void findSMSByTos() {
+    public void findSMSByTosTest() {
+        smsServiceInterface.sentAndSaveSMS(expectedSMS);
+        int sizeList = smsServiceInterface.findSMSByTos(expectedSMS.getTos()).size();
+        assertEquals(sizeList,smsServiceInterface.findSMSByTos(expectedSMS.getTos()).size());
     }
 }
